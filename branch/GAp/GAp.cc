f7b4f0ca51ae67405d00509b9b1d202ff3700c95
Bryanskiy <ivakin.kir@gmail.com>
2024-05-18 22:04:02 +0300

    GAp fixes

--- branch/GAp/GAp.cc
+++ branch/GAp/GAp.cc
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <array>
 #include <bitset>
+#include <iostream>
 #include <map>
 
 #include "msl/fwcounter.h"
@@ -10,9 +11,9 @@ namespace
 {
 constexpr std::size_t GLOBAL_HISTORY_LENGTH = 14;
 constexpr std::size_t COUNTER_BITS = 2;
-constexpr std::size_t GAP_TABLE_SIZE = 16384; // 2^14
-constexpr std::size_t PER_ADDR_PHT_SIZE = 32; // 2^5
-constexpr std::size_t MASK = 0x5;
+constexpr std::size_t GAP_TABLE_SIZE = 16384;  // 2^14
+constexpr std::size_t PER_ADDR_PHT_SIZE = 128; // 2^7
+constexpr std::size_t MASK = 0x0000000000000007;
 
 using PHT = std::array<std::array<champsim::msl::fwcounter<COUNTER_BITS>, GAP_TABLE_SIZE>, PER_ADDR_PHT_SIZE>;
 std::map<O3_CPU*, PHT> gap_history_table;
@@ -29,7 +30,7 @@ uint8_t O3_CPU::predict_branch(uint64_t ip)
   std::size_t history_table_idx = history_register.to_ullong();
 
   // load pattern from history table
-  auto value = ::gap_history_table[this][ip && MASK][history_table_idx];
+  auto value = ::gap_history_table[this][ip & MASK][history_table_idx];
 
   // predict
   return value.value() >= (value.maximum / 2);
@@ -41,7 +42,7 @@ void O3_CPU::last_branch_result(uint64_t ip, uint64_t branch_target, uint8_t tak
   auto&& history_register = branch_history_register[this];
   std::size_t history_table_idx = history_register.to_ullong();
 
-  ::gap_history_table[this][ip && MASK][history_table_idx] += taken ? 1 : -1;
+  ::gap_history_table[this][ip & MASK][history_table_idx] += taken ? 1 : -1;
 
   // update branch history register
   ::branch_history_register[this] <<= 1;
