c2b53e23673eba19df57b6287f1851571c3a2be3
Bryanskiy <ivakin.kir@gmail.com>
2024-05-18 21:07:38 +0300

    implement GAp

--- branch/GAg/GAg.cc
+++ branch/GAg/GAg.cc
@@ -5,7 +5,7 @@ namespace
 {
 constexpr std::size_t GLOBAL_HISTORY_LENGTH = 14;
 constexpr std::size_t COUNTER_BITS = 2;
-constexpr std::size_t GAG_TABLE_SIZE = 16384;
+constexpr std::size_t GAG_TABLE_SIZE = 16384; // 2^14
 
 std::map<O3_CPU*, std::bitset<GLOBAL_HISTORY_LENGTH>> branch_history_register;
 std::map<O3_CPU*, std::array<champsim::msl::fwcounter<COUNTER_BITS>, GAG_TABLE_SIZE>> gag_history_table;
@@ -17,7 +17,7 @@ uint8_t O3_CPU::predict_branch(uint64_t ip)
 {
   // load global register
   std::size_t history_register = branch_history_register[this];
-  std::size_t history_table_idx = history_register.to_ullong() % GAG_TABLE_SIZE;
+  std::size_t history_table_idx = history_register.to_ullong();
 
   // load pattern from history table
   auto value = ::gag_history_table[this][history_table_idx];
@@ -30,7 +30,7 @@ void O3_CPU::last_branch_result(uint64_t ip, uint64_t branch_target, uint8_t tak
 {
   // update history table
   std::size_t history_register = branch_history_register[this];
-  std::size_t history_table_idx = history_register.to_ullong() % GAG_TABLE_SIZE;
+  std::size_t history_table_idx = history_register.to_ullong();
   ::gag_history_table[this][history_table_idx] += taken ? 1 : -1;
 
   // update branch history register
--- branch/GAp/GAp.cc
+++ branch/GAp/GAp.cc
@@ -5,9 +5,14 @@ namespace
 {
 constexpr std::size_t GLOBAL_HISTORY_LENGTH = 14;
 constexpr std::size_t COUNTER_BITS = 2;
-constexpr std::size_t GAG_TABLE_SIZE = 16384;
+constexpr std::size_t GAP_TABLE_SIZE = 16384; // 2^14
+constexpr std::size_t PER_ADDR_PHT_SIZE = 32; // 2^5
+constexpr std::size_t MASK = 0x5;
 
+using PHT = std::array<std::array<champsim::msl::fwcounter<COUNTER_BITS>, BIMODAL_TABLE_SIZE>, PER_ADDR_PHT_SIZE>;
+std::map<O3_CPU*, PHT> gap_history_table;
 std::map<O3_CPU*, std::bitset<GLOBAL_HISTORY_LENGTH>> branch_history_register;
+
 } // namespace
 
 void O3_CPU::initialize_branch_predictor() {}
@@ -16,10 +21,10 @@ uint8_t O3_CPU::predict_branch(uint64_t ip)
 {
   // load global register
   std::size_t history_register = branch_history_register[this];
-  std::size_t history_table_idx = history_register.to_ullong() % GAG_TABLE_SIZE;
+  std::size_t history_table_idx = history_register.to_ullong();
 
   // load pattern from history table
-  auto value = ::gag_history_table[this][history_table_idx];
+  auto value = ::gag_history_table[this][ip && MASK][history_table_idx];
 
   // predict
   return value.value() >= (value.maximum / 2);
@@ -27,6 +32,11 @@ uint8_t O3_CPU::predict_branch(uint64_t ip)
 
 void O3_CPU::last_branch_result(uint64_t ip, uint64_t branch_target, uint8_t taken, uint8_t branch_type)
 {
+  // update history table
+  std::size_t history_register = branch_history_register[this];
+  std::size_t history_table_idx = history_register.to_ullong();
+
+  ::gap_history_table[this][ip && MASK][history_table_idx] += taken ? 1 : -1;
 
   // update branch history register
   ::branch_history_register[this] <<= 1;
